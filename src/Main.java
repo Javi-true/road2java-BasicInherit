8c13f0071fe80747c9334a6d598540da30aa2d47
Javier Trujillo <rivera.dev@outlook.com>
2024-12-12 05:35:15 -0600

    Agregado ejemplo basico de metodo static

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 
 public class Main {
 
+    //ejemplo basico de metodo static.
+    public static void saludar(){
+        System.out.println("Bienvenido al sistema universtario");
+    }
+
     public static void main(String[] args){
 
+        Main.saludar();
         Alumno alumno_1 = new Alumno("Javier Trujillo", 29, 20096110);
         alumno_1.mostrarInformacion();
         alumno_1.estudiar();
